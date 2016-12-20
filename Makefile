dc148c1ece62eae6253c4e15ec6565ac215d9e1f
Santiago Pagola <santiago.pagola@alten.se>
2016-12-20 16:21:21 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,8 @@
+all:	main
+
+main:	main.o
+
+main.o:	main.c
+
+clean:
+	rm -rf *.o *~ main
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int nr;
+	printf("Nr: ");
+	scanf(" %d", &nr);
+	
+	printf("There %s %d error%s\n", (nr>1 || nr == 0) ? "are":"is just"
+				      , nr
+				      , (nr > 1 || nr == 0) ? "s":"");
+	return 0;
+}
